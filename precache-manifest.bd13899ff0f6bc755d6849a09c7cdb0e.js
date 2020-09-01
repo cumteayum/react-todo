2ac9d0dd5fc34ba2c82bf7a5e2536f0ee0584cac
ishan <harddgamerx@gmail.com>
2020-09-01 17:26:52 +0530

    Updates

---
+++ precache-manifest.bd13899ff0f6bc755d6849a09c7cdb0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "d1a87594319f8cdddcbd",
+    "url": "/react-todo/static/js/main.515abe14.chunk.js"
+  },
+  {
+    "revision": "f40809dcdf0fac3af8a1",
+    "url": "/react-todo/static/js/2.98ee9b8f.chunk.js"
+  },
+  {
+    "revision": "d1a87594319f8cdddcbd",
+    "url": "/react-todo/static/css/main.5ea5e48d.chunk.css"
+  },
+  {
+    "revision": "dc6f951642643ec3395c482089157ead",
+    "url": "/react-todo/index.html"
+  }
+];
